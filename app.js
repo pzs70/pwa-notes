900c5a470065939fb9923e36ae8a8f4d08e44a93
pzs70 <33121091+pzs70@users.noreply.github.com>
2025-08-22 11:13:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ document.querySelectorAll(".nav-links a").forEach(link => {
       loadReadme();
     } else if (target === "lista") {
       loadSheetData();
+      console.log("Sheet data loaded");
     }
 
     // Menü becsukása mobilon
