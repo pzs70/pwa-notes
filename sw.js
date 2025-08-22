ee2328ce4bce882487273f680b1500d8f01345c4
pzs70 <33121091+pzs70@users.noreply.github.com>
2025-08-22 10:44:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 const CACHE_NAME = "pwa-notes-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/README.md"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./README.md",
+  "https://cdn.jsdelivr.net/npm/marked/marked.min.js"
 ];
 
 // Telepítéskor cache feltöltése
